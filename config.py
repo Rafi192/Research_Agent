f706e0d8473ad45909b01d5d18e87a23f2e3ae86
rafi192 <rafihasanmmsc@gmail.com>
2026-02-23 10:50:07 +0600

    Commit of research agent config

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+openai_api_key = os.getenv("OPENAI_API_KEY")
+TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
+
+MODEL_GPT = "gpt-4o"
+MODEL_GEMINI = 'gemini-2.5-flash'
+temperature = 0.7
+MAX_TOKENS = 4096
+
+# agent settings
+AGENT_NAME = "RAFIs Research Assistant"
+AGENT_GOAL = "Assist Rafi in conducting research by gathering information, analyzing data, and providing insights to support informed decision-making."
+
+MAX_ITERATIONS = 10
+MAX_SEARCH_RESULT = 5
+
+
+output_dir = 'outputs'
+os.makedirs(output_dir, exist_ok=True)
